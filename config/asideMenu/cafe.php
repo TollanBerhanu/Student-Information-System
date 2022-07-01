a0468cf876f5c0fedcadd4e5837331aa2b946f75
awol <awolabdulbaasit143@gmail.com>
2022-07-01 01:58:33 -0700

    all cafe stracture some part is done

--- config/asideMenu/cafe.php
+++ config/asideMenu/cafe.php
@@ -8,7 +8,7 @@ return [
             'title' => 'Cafe Admin Dashbord',
             'root' => true,
             'icon' => 'media/svg/icons/Food/Dinner.svg', // or can be 'flaticon-home' or any flaticon-*
-            'page' => '#',
+            'page' => '/cafe/cafeAdminDashboard',
             'new-tab' => false,
         ],
     ]
--- routes/webRoutes/CafeRoutes.php
+++ routes/webRoutes/CafeRoutes.php
@@ -5,5 +5,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => '/cafe'], function (){
 
-    Route::get('', 'Cafe\CafePagesController@index')->middleware(['auth'])->name('cafeDashboard');
+    Route::get('', 'Cafe\CafePagesController@index')->middleware(['auth'])->name('cafePage');
+    Route::get('cafeAdminDashboard','Cafe\CafePagesController@cafeDashboard')->middleware(['auth','privilege:cafe_admin_dashboard'])->name("cafeDashboard");
+});
+
+Route::group(['prefix' => '/cafe/Opration'], function (){
+
+    Route::get('cafe_register_update_page', 'Cafe\CafePagesController@cafe_register')->middleware(['auth','privilege:cafe_register_update']);
 });
